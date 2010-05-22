8d88e41bcd80dcaa88c124a7a2fa3ab5b9802c75
adlermedrado <adler@adlermedrado.com.br>
2010-05-22 15:56:37 -0300

    Adicionando Zend_Form ao projeto.

--- application/controllers/UsersController.php
+++ application/controllers/UsersController.php
@@ -11,7 +11,18 @@ class UsersController extends Zend_Controller_Action
     public function indexAction()
     {
         // action body
-		$this->view->minhaFrase = "Os programadores são a Elite da TI";
+		$this->view->minhaFrase = "Os programadores são a Elite da TI <script>// oi amigao </script>";
+		$this->view->form = new Application_Form_User();
+		
+		if ($this->getRequest()->isPost()) {
+			if ($this->view->form->isValid($this->_request->getPost())) {
+				// Salva no banco de dados ou seja lá o que for fazer com os dados provenientes do form
+				var_dump($this->_request->getPost());
+			} else {
+				$this->view->form->populate($this->_request->getPost());
+			}
+		}
+		
     }
 
 
